da16ddebec2b7b3ad7e4e7583a412f3b3b182b4a
oscar-diaz <oscarpucp@gmail.com>
2024-11-15 02:46:51 -0500

    Clase 13 - Gateway y Resiliencia

---
+++ src/main/java/com/example/clase13_apigateway/Clase13ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.clase13_apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clase13ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clase13ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clase13_apigateway/Clase13ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clase13_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clase13ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
